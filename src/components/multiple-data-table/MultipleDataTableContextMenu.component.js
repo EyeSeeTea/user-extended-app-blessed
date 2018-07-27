93bd2701c4036328ebb72ca9f5811a3fa225cf9a
Arnau Sanchez <pyarnau@gmail.com>
2018-07-27 13:15:22 +0200

    Add support for subactions

--- src/components/multiple-data-table/MultipleDataTableContextMenu.component.js
+++ src/components/multiple-data-table/MultipleDataTableContextMenu.component.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Translate from 'd2-ui/lib/i18n/Translate.mixin';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
+import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
 import FontIcon from 'material-ui/FontIcon';
 import Popover from 'material-ui/Popover';
 import Paper from 'material-ui/Paper';
@@ -9,6 +10,7 @@ import Paper from 'material-ui/Paper';
 const MultipleDataTableContextMenu = React.createClass({
     propTypes: {
         actions: React.PropTypes.objectOf(React.PropTypes.func),
+        subactions: React.PropTypes.object,
         showContextMenu: React.PropTypes.bool,
         activeItems: React.PropTypes.array,
         icons: React.PropTypes.object,
@@ -32,7 +34,7 @@ const MultipleDataTableContextMenu = React.createClass({
         const cmStyle = {
             position: 'fixed',
         };
-        const {actions, target, activeItems, icons, showContextMenu, ...popoverProps} = this.props;
+        const {actions, subactions, target, activeItems, icons, showContextMenu, ...popoverProps} = this.props;
 
         return (
             <Popover
@@ -47,13 +49,24 @@ const MultipleDataTableContextMenu = React.createClass({
                 <Menu className="data-table__context-menu" desktop>
                     {actionList.map((action) => {
                         const iconName = icons[action] ? icons[action] : action;
+                        const subactionItems = subactions[action];
+                        const menuItems = subactionItems ?
+                            subactionItems.map(item =>
+                                <MenuItem
+                                    primaryText={this.getTranslation(item.name)}
+                                    onClick={() => this.runSubactionItem(item)}
+                                />
+                            ) : null;
 
                         return (<MenuItem key={action}
                                           data-object-id={activeItems}
                                           className={'data-table__context-menu__item'}
-                                          onClick={this.handleClick.bind(this, action)}
+                                          onClick={!menuItems ? this.handleClick.bind(this, action) : undefined}
                                           primaryText={this.getTranslation(action)}
                                           leftIcon={<FontIcon className="material-icons">{iconName}</FontIcon>}
+
+                                          rightIcon={menuItems ? <ArrowDropRight /> : undefined}
+                                          menuItems={menuItems}
                                 />);
                     })}
                 </Menu>
@@ -65,6 +78,12 @@ const MultipleDataTableContextMenu = React.createClass({
         this.props.actions[action].apply(this.props.actions, this.props.activeItems);
         this.props.onRequestClose && this.props.onRequestClose();
     },
+
+    runSubactionItem(item) {
+        const {activeItems} = this.props;
+        item.onClick(activeItems);
+        this.props.onRequestClose && this.props.onRequestClose();
+    }
 });
 
 export default MultipleDataTableContextMenu;
