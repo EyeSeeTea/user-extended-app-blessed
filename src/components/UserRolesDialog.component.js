eeba86bdd03c5c8c9f49bdcb3739bb989335e07e
Alexis Rico <sferadev@gmail.com>
2019-11-10 20:25:24 +0100

    Do not clear lastLogin date on updating user roles

--- src/components/UserRolesDialog.component.js
+++ src/components/UserRolesDialog.component.js
@@ -28,7 +28,7 @@ function UserRolesDialog(props, context) {
 
     const modelOptions = {
         parentModel: d2.models.users,
-        parentFields: ":owner,userCredentials[id,username,userRoles[id,name]]",
+        parentFields: ":owner,userCredentials[id,username,userRoles[id,name],lastLogin]",
         childrenModel: d2.models.userRoles,
         getChildren: user => user.userCredentials.userRoles,
         getPayload: getPayload,
