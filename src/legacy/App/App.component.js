9733cf577dde624b97d5f72d69d40aa8a0b0c3ec
Alexis Rico <sferadev@gmail.com>
2021-11-11 13:47:30 +0100

    Remove old app subscription
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- src/legacy/App/App.component.js
+++ src/legacy/App/App.component.js
@@ -4,11 +4,9 @@ import SinglePanelLayout from "d2-ui/lib/layout/SinglePanel.component";
 import { getInstance } from "d2/lib/d2";
 import PropTypes from "prop-types";
 import React from "react";
-import { Observable } from "rx";
 import LoadingMask from "../loading-mask/LoadingMask.component";
 import SnackbarContainer from "../Snackbar/SnackbarContainer.component";
 import appTheme from "./app.theme";
-import appState from "./appStateStore";
 
 const withMuiContext = Object.assign(AppWithD2.childContextTypes, { muiTheme: PropTypes.object });
 
@@ -18,38 +16,6 @@ class App extends AppWithD2 {
             muiTheme: appTheme,
         });
     }
-
-    componentDidMount = () => {
-        super.componentDidMount();
-
-        // The all section is a special section that should not be treated like a normal section as it does not
-        // have the sidebar. It is used to display the collection of all meta data objects. The all section will
-        // therefore always emit false.
-        const allSectionSelected$ = appState.filter(state => state.sideBar.currentSection === "all").map(() => false);
-
-        const nonAllSectionSelected$ = appState
-            // The all section is managed separately so we do not want to process those any further
-            .filter(state => state.sideBar.currentSection !== "all")
-            .map(state =>
-                // Check if the current section is in the list of mainSections
-                state.mainSections.some(mainSection => mainSection.key === state.sideBar.currentSection)
-            );
-
-        this.disposable = Observable.merge(allSectionSelected$, nonAllSectionSelected$)
-            // Do not emit the value more often than needed to prevent unnecessary react triggers
-            .distinctUntilChanged()
-            .subscribe(hasSection =>
-                this.setState({
-                    ...this.state,
-                    hasSection,
-                })
-            );
-    };
-
-    componentWillUnmount = () => {
-        super.componentWillUnmount();
-        this.disposable.dispose?.();
-    };
 
     render = () => {
         if (!this.state.d2) {
