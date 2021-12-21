b4bc6edd35607af71732e35852eecb507a51a789
p3rcypj <pablo@percypj.com>
2021-12-21 13:01:11 +0100

    Prettier

--- src/data/clients/storage/DataStoreStorageClient.ts
+++ src/data/clients/storage/DataStoreStorageClient.ts
@@ -7,19 +7,29 @@ import { dataStoreNamespace } from "./Namespaces";
 import { StorageClient } from "./StorageClient";
 
 export class DataStoreStorageClient extends StorageClient {
-    private api:D2Api;
-    private dataStore:DataStore;
+    private api: D2Api;
+    private dataStore: DataStore;
 
-    constructor(type:"user"|"global",instance:Instance){
+    constructor(type: "user" | "global", instance: Instance) {
         super();
-        this.api=getD2APiFromInstance(instance);
-        this.dataStore=type==="user"?this.api.userDataStore(dataStoreNamespace):this.api.dataStore(dataStoreNamespace);}
+        this.api = getD2APiFromInstance(instance);
+        this.dataStore =
+            type === "user" ? this.api.userDataStore(dataStoreNamespace) : this.api.dataStore(dataStoreNamespace);
+    }
 
-    public getObject<T extends object>(key:string):FutureData<T|undefined>{return apiToFuture(this.dataStore.get<T>(key));}
-    public getOrCreateObject<T extends object>(key:string,defaultValue:T):FutureData<T> {
-        return this.getObject<T>(key).flatMap(value=>{
-          if(!value) return this.saveObject(key,defaultValue).map(()=>defaultValue)
-          return Future.success(value)});}
-    public saveObject<T extends object>(key:string,value:T):FutureData<void> {return apiToFuture(this.dataStore.save(key,value));}
-    public removeObject(key:string):FutureData<void> {return apiToFuture(this.dataStore.delete(key)).map(()=>{});}
+    public getObject<T extends object>(key: string): FutureData<T | undefined> {
+        return apiToFuture(this.dataStore.get<T>(key));
+    }
+    public getOrCreateObject<T extends object>(key: string, defaultValue: T): FutureData<T> {
+        return this.getObject<T>(key).flatMap(value => {
+            if (!value) return this.saveObject(key, defaultValue).map(() => defaultValue);
+            return Future.success(value);
+        });
+    }
+    public saveObject<T extends object>(key: string, value: T): FutureData<void> {
+        return apiToFuture(this.dataStore.save(key, value));
+    }
+    public removeObject(key: string): FutureData<void> {
+        return apiToFuture(this.dataStore.delete(key)).map(() => {});
+    }
 }
--- src/data/clients/storage/StorageClient.ts
+++ src/data/clients/storage/StorageClient.ts
@@ -4,65 +4,97 @@ import { Ref } from "../../../domain/entities/Ref";
 import { Namespace, NamespaceProperties } from "./Namespaces";
 
 export abstract class StorageClient {
-  // Object operations
-  public abstract getObject<T extends object>(key:string):FutureData<T|undefined>;
-  public abstract getOrCreateObject<T extends object>(key:string,defaultValue:T):FutureData<T>;
-  public abstract saveObject<T extends object>(key:string,value:T):FutureData<void>;
-  public abstract removeObject(key:string):FutureData<void>;
+    // Object operations
+    public abstract getObject<T extends object>(key: string): FutureData<T | undefined>;
+    public abstract getOrCreateObject<T extends object>(key: string, defaultValue: T): FutureData<T>;
+    public abstract saveObject<T extends object>(key: string, value: T): FutureData<void>;
+    public abstract removeObject(key: string): FutureData<void>;
 
-  public listObjectsInCollection<T extends Ref>(key:string):FutureData<T[]>{
-    return this.getObject<T[]>(key).map(value=>value??[])}
+    public listObjectsInCollection<T extends Ref>(key: string): FutureData<T[]> {
+        return this.getObject<T[]>(key).map(value => value ?? []);
+    }
 
-  public getObjectInCollection<T extends Ref>(key:string,id:string):FutureData<T|undefined> {
-    return this.getObject<T[]>(key).map(value=>value??[]).flatMap(rawData=>{
-      const baseElement=_.find(rawData,element=>element.id===id);
-        if (!baseElement) return Future.success(undefined);
-        const advancedProperties=NamespaceProperties[key]??[];
-        if(advancedProperties.length>0) {
-          return this.getObject(`${key}-${id}`).map(value=>{
-            if(!value) return baseElement;
-            return {...baseElement,...value} as T;})}
-        return Future.success(baseElement);});}
+    public getObjectInCollection<T extends Ref>(key: string, id: string): FutureData<T | undefined> {
+        return this.getObject<T[]>(key)
+            .map(value => value ?? [])
+            .flatMap(rawData => {
+                const baseElement = _.find(rawData, element => element.id === id);
+                if (!baseElement) return Future.success(undefined);
+                const advancedProperties = NamespaceProperties[key] ?? [];
+                if (advancedProperties.length > 0) {
+                    return this.getObject(`${key}-${id}`).map(value => {
+                        if (!value) return baseElement;
+                        return { ...baseElement, ...value } as T;
+                    });
+                }
+                return Future.success(baseElement);
+            });
+    }
 
-  public saveObjectsInCollection<T extends Ref>(key:Namespace,elements:T[]):FutureData<void> {
-    const advancedProperties=NamespaceProperties[key]??[];
-    const baseElements=elements.map(element=>_.omit(element,advancedProperties));
-    return this.getObject<T[]>(key).map(value=><Ref[]>(value??[])).map(oldData=>{
-      const cleanData=oldData.filter(item=>!elements.some(element=>item.id===element.id));
-      // Save base elements directly into collection: model
-      this.saveObject(key,[...cleanData,...baseElements]).map(()=>{
-        // Save advanced properties to its own key: model-id
-        if(advancedProperties.length>0){
-          for(const element of elements){
-            const advancedElement=_.pick(element,advancedProperties);
-            this.saveObject(`${key}-${element.id}`,advancedElement);}}});});}
+    public saveObjectsInCollection<T extends Ref>(key: Namespace, elements: T[]): FutureData<void> {
+        const advancedProperties = NamespaceProperties[key] ?? [];
+        const baseElements = elements.map(element => _.omit(element, advancedProperties));
+        return this.getObject<T[]>(key)
+            .map(value => <Ref[]>(value ?? []))
+            .map(oldData => {
+                const cleanData = oldData.filter(item => !elements.some(element => item.id === element.id));
+                // Save base elements directly into collection: model
+                this.saveObject(key, [...cleanData, ...baseElements]).map(() => {
+                    // Save advanced properties to its own key: model-id
+                    if (advancedProperties.length > 0) {
+                        for (const element of elements) {
+                            const advancedElement = _.pick(element, advancedProperties);
+                            this.saveObject(`${key}-${element.id}`, advancedElement);
+                        }
+                    }
+                });
+            });
+    }
 
-  public saveObjectInCollection<T extends Ref>(key:Namespace,element:T):FutureData<void> {
-    const advancedProperties=NamespaceProperties[key]??[];
-    const baseElement=_.omit(element,advancedProperties);
-    return this.getObject<T[]>(key).map(value=><Ref[]>(value??[])).map(oldData=>{
-      const foundIndex=_.findIndex(oldData,item=>item.id===element.id);
-      const arrayIndex=foundIndex===-1?oldData.length:foundIndex;
-      // Save base element directly into collection: model
-      this.saveObject(key,[...oldData.slice(0,arrayIndex),baseElement,...oldData.slice(arrayIndex+1)]).map(()=>{
-        // Save advanced properties to its own key: model-id
-        if(advancedProperties.length>0) {
-          const advancedElement=_.pick(element,advancedProperties);
-          this.saveObject(`${key}-${element.id}`,advancedElement);}});});}
+    public saveObjectInCollection<T extends Ref>(key: Namespace, element: T): FutureData<void> {
+        const advancedProperties = NamespaceProperties[key] ?? [];
+        const baseElement = _.omit(element, advancedProperties);
+        return this.getObject<T[]>(key)
+            .map(value => <Ref[]>(value ?? []))
+            .map(oldData => {
+                const foundIndex = _.findIndex(oldData, item => item.id === element.id);
+                const arrayIndex = foundIndex === -1 ? oldData.length : foundIndex;
+                // Save base element directly into collection: model
+                this.saveObject(key, [
+                    ...oldData.slice(0, arrayIndex),
+                    baseElement,
+                    ...oldData.slice(arrayIndex + 1),
+                ]).map(() => {
+                    // Save advanced properties to its own key: model-id
+                    if (advancedProperties.length > 0) {
+                        const advancedElement = _.pick(element, advancedProperties);
+                        this.saveObject(`${key}-${element.id}`, advancedElement);
+                    }
+                });
+            });
+    }
 
-  public removeObjectInCollection(key:string,id:string):FutureData<void> {
-    return this.getObject(key).map(value=><Ref[]>(value??[])).map(oldData=>{
-      const newData=_.reject(oldData,{id});
-      this.saveObject(key,newData).map(()=>{
-        const advancedProperties=NamespaceProperties[key]??[];
-        if(advancedProperties.length>0) this.removeObject(`${key}-${id}`);})});}
+    public removeObjectInCollection(key: string, id: string): FutureData<void> {
+        return this.getObject(key)
+            .map(value => <Ref[]>(value ?? []))
+            .map(oldData => {
+                const newData = _.reject(oldData, { id });
+                this.saveObject(key, newData).map(() => {
+                    const advancedProperties = NamespaceProperties[key] ?? [];
+                    if (advancedProperties.length > 0) this.removeObject(`${key}-${id}`);
+                });
+            });
+    }
 
-  public removeObjectsInCollection(key:string,ids:string[]):FutureData<void> {
-    return this.getObject(key).map(value=><Ref[]>(value??[])).map(oldData=>{
-      const newData=_.reject(oldData,({id})=>ids.includes(id));
-      this.saveObject(key,newData).map(()=>{
-        const advancedProperties=NamespaceProperties[key]??[];
-        if(advancedProperties.length>0)
-          for(const id of ids) this.removeObject(`${key}-${id}`);})});}
-
+    public removeObjectsInCollection(key: string, ids: string[]): FutureData<void> {
+        return this.getObject(key)
+            .map(value => <Ref[]>(value ?? []))
+            .map(oldData => {
+                const newData = _.reject(oldData, ({ id }) => ids.includes(id));
+                this.saveObject(key, newData).map(() => {
+                    const advancedProperties = NamespaceProperties[key] ?? [];
+                    if (advancedProperties.length > 0) for (const id of ids) this.removeObject(`${key}-${id}`);
+                });
+            });
+    }
 }
