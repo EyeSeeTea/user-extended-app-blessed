e8d06356aba7ad2b9aea963d7067bc9a4529acb9
nshandra <34254522+nshandra@users.noreply.github.com>
2022-02-17 03:48:17 +0100

    Added Language and DBlang fields. Added password and phoneNumber validators.

--- src/webapp/components/user-form/UserForm.tsx
+++ src/webapp/components/user-form/UserForm.tsx
@@ -8,6 +8,7 @@ import {
     string,
     alphaNumeric,
     CheckboxFieldFF,
+    SingleSelectFieldFF,
 } from "@dhis2/ui";
 import React from "react";
 import i18n from "../../../locales";
@@ -15,11 +16,10 @@ import { fullUidRegex } from "../../../utils/uid";
 import { FormField } from "../form/fields/FormField";
 import { PreviewInputFF } from "../form/fields/PreviewInputFF";
 import { UserRoleGroupFF } from "./components/UserRoleGroupFF";
-import { getUserFieldName, UserFormField, userRequiredFields } from "./utils";
+import { getUserFieldName, UserFormField, userRequiredFields, uiLocaleFields, dbLocaleFields } from "./utils";
 import { OrgUnitsSelector } from "@eyeseetea/d2-ui-components";
 import { useAppContext } from "../../contexts/app-context";
 
-// TODO: new fields validators
 const useValidations = (field: UserFormField): { validation?: (...args: any[]) => any; props?: object } => {
     switch (field) {
         case "id":
@@ -35,12 +35,40 @@ const useValidations = (field: UserFormField): { validation?: (...args: any[]) =
             return {
                 validation: composeValidators(alphaNumeric, createMinCharacterLength(1), createMaxCharacterLength(255)),
             };
+        // Why not use @dhis2/ui email validator?
         case "email":
             return {
                 validation: createPattern(
                     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
                     i18n.t("Please provide a valid email")
                 ),
+            };
+        // TODO: Password length is admin set option /api/systemSettings/minPasswordLength
+        case "password":
+            return {
+                validation: composeValidators(
+                    string, createMinCharacterLength(8), createMaxCharacterLength(255),
+                    createPattern(/.*[a-z]/, i18n.t("Password should contain at least one lowercase letter")),
+                    createPattern(/.*[A-Z]/, i18n.t("Password should contain at least one UPPERCASE letter")),
+                    createPattern(/.*[0-9]/, i18n.t("Password should contain at least one number")),
+                    createPattern(/[^A-Za-z0-9]/, i18n.t("Password should have at least one special character"))
+                    ),
+            };
+        case "phoneNumber":
+            return {
+                validation:
+                    createPattern(
+                        /^\+?[0-9 \-()]+$/,
+                        i18n.t("Please provide a valid phone number")
+                        ),
+            };
+        case "whatsApp":
+            return {
+                validation:
+                    createPattern(
+                        /^\+[0-9 ]+$/,
+                        i18n.t("Please provide a valid international phone number (+0123456789)")
+                        ),
             };
         default: {
             const required = userRequiredFields.includes(field);
@@ -94,6 +122,7 @@ export const RenderUserWizardField: React.FC<{ row: number; field: UserFormField
                     {...props}
                     component={OrgUnitsSelector}
                     api={api}
+                    // selected={}
                     controls={{
                         filterByLevel: true,
                         filterByGroup: true,
@@ -105,6 +134,10 @@ export const RenderUserWizardField: React.FC<{ row: number; field: UserFormField
         case "externalAuth":
         case "disabled":
             return <FormField {...props} component={CheckboxFieldFF} type={"checkbox"} />;
+        case "uiLocale":
+            return <FormField {...props} component={SingleSelectFieldFF} options={uiLocaleFields} />;   
+        case "dbLocale":
+            return <FormField {...props} component={SingleSelectFieldFF} options={dbLocaleFields} />;   
         default:
             return null;
     }
--- src/webapp/components/user-form/utils.ts
+++ src/webapp/components/user-form/utils.ts
@@ -23,11 +23,91 @@ export const userFormFields = [
     "externalAuth",
     "openId",
     "ldapId",
+    "uiLocale",
+    "dbLocale",
     "apiUrl",
     "dataViewOrganisationUnits",
     "organisationUnits",
     "userGroups",
     "userRoles",
+];
+
+// TODO: Temporal for form testing, should be retrieved programaticly from the api
+export const uiLocaleFields = [
+    { value: "ar", label: i18n.t("Arabic") },
+    { value: "ar_EG", label: i18n.t("Arabic (Egypt)") },
+    { value: "ar_IQ", label: i18n.t("Arabic (Iraq)") },
+    { value: "ar_SD", label: i18n.t("Arabic (Sudan)") },
+    { value: "bn", label: i18n.t("Bengali") },
+    { value: "bi", label: i18n.t("Bislama") },
+    { value: "my", label: i18n.t("Burmese") },
+    { value: "zh", label: i18n.t("Chinese") },
+    { value: "zh_CN", label: i18n.t("Chinese (China)") },
+    { value: "cs", label: i18n.t("Czech") },
+    { value: "da", label: i18n.t("Danish") },
+    { value: "en", label: i18n.t("English") },
+    { value: "fr", label: i18n.t("French") },
+    { value: "in", label: i18n.t("Indonesian") },
+    { value: "in_ID", label: i18n.t("Indonesian (Indonesia)") },
+    { value: "km", label: i18n.t("Khmer") },
+    { value: "rw", label: i18n.t("Kinyarwanda") },
+    { value: "lo", label: i18n.t("Lao") },
+    { value: "mn", label: i18n.t("Mongolian") },
+    { value: "ne", label: i18n.t("Nepali") },
+    { value: "nb", label: i18n.t("Norwegian Bokmål") },
+    { value: "pt", label: i18n.t("Portuguese") },
+    { value: "pt_BR", label: i18n.t("Portuguese (Brazil)") },
+    { value: "ps", label: i18n.t("Pushto") },
+    { value: "ru", label: i18n.t("Russian") },
+    { value: "es", label: i18n.t("Spanish") },
+    { value: "sv", label: i18n.t("Swedish") },
+    { value: "tg", label: i18n.t("Tajik") },
+    { value: "tet", label: i18n.t("Tetum") },
+    { value: "uk", label: i18n.t("Ukrainian") },
+    { value: "ur", label: i18n.t("Urdu") },
+    { value: "uz", label: i18n.t("Uzbek") },
+    { value: "vi", label: i18n.t("Vietnamese") },
+    { value: "ckb", label: i18n.t("ckb") },
+    { value: "prs", label: i18n.t("prs") },
+];
+
+export const dbLocaleFields = [
+    { value: "af", label: i18n.t("Afrikaans") },
+    { value: "am", label: i18n.t("Amharic") },
+    { value: "ar", label: i18n.t("Arabic") },
+    { value: "bi", label: i18n.t("Bislama") },
+    { value: "my", label: i18n.t("Burmese") },
+    { value: "zh", label: i18n.t("Chinese") },
+    { value: "nl", label: i18n.t("Dutch") },
+    { value: "dz", label: i18n.t("Dzongkha") },
+    { value: "en", label: i18n.t("English") },
+    { value: "en_BW", label: i18n.t("English (Botswana)") },
+    { value: "en_KH", label: i18n.t("English (Cambodia)") },
+    { value: "en_TZ", label: i18n.t("English (Tanzania)") },
+    { value: "en_UG", label: i18n.t("English (Uganda)") },
+    { value: "fr", label: i18n.t("French") },
+    { value: "fr_SN", label: i18n.t("French (Senegal)") },
+    { value: "fr_SN", label: i18n.t("French (Senegal)") },
+    { value: "de", label: i18n.t("German") },
+    { value: "gu", label: i18n.t("Gujarati") },
+    { value: "hi", label: i18n.t("Hindi") },
+    { value: "in", label: i18n.t("Indonesian") },
+    { value: "it", label: i18n.t("Italian") },
+    { value: "km", label: i18n.t("Khmer") },
+    { value: "rw", label: i18n.t("Kinyarwanda") },
+    { value: "lo", label: i18n.t("Lao") },
+    { value: "ne", label: i18n.t("Nepali") },
+    { value: "no", label: i18n.t("Norwegian") },
+    { value: "fa", label: i18n.t("Persian") },
+    { value: "pt", label: i18n.t("Portuguese") },
+    { value: "ps", label: i18n.t("Pushto") },
+    { value: "ru", label: i18n.t("Russian") },
+    { value: "es", label: i18n.t("Spanish") },
+    { value: "es_GT", label: i18n.t("Spanish (Guatemala)") },
+    { value: "es_MX", label: i18n.t("Spanish (Mexico)") },
+    { value: "sw", label: i18n.t("Swahili") },
+    { value: "tg", label: i18n.t("Tajik") },
+    { value: "vi", label: i18n.t("Vietnamese") },
 ];
 
 export const userRequiredFields: UserFormField[] = ["id", "name"];
@@ -78,6 +158,10 @@ export const getUserName = (field: UserFormField) => {
             return i18n.t("Open ID");
         case "ldapId":
             return i18n.t("LDAP identifier");
+        case "uiLocale":
+            return i18n.t("Interface language");
+        case "dbLocale":
+            return i18n.t("Database language");
         case "apiUrl":
             return i18n.t("Api URL");
     }
