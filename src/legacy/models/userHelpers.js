46f078a45d629c65b53e953a4f3d9badfe677f5b
MarieJousse <104760844+MarieJousse@users.noreply.github.com>
2022-06-24 13:52:22 +0200

    Update userHelpers.js

--- src/legacy/models/userHelpers.js
+++ src/legacy/models/userHelpers.js
@@ -451,6 +451,18 @@ async function exportTemplateToCsv() {
     return Papa.unparse(table);
 }
 
+/* Get users from Dhis2 API and export given columns to a Json string */
+async function exportToJson (d2, columns, filterOptions, { orgUnitsField }) {
+    const { filters, ...listOptions } = { ...filterOptions, pageSize: 1e6 };
+    const { users } = await getUserList(d2, filters, listOptions);
+    const userRows = users.map(user => _.at(getPlainUser(user, { orgUnitsField }), columns));
+    const header = columns.map(getColumnNameFromProperty);
+    const table = [header, ...userRows];
+
+    return Papa.unparse(table);
+}
+ 
+
 async function importFromCsv(d2, file, { maxUsers, orgUnitsField }) {
     return new Promise((resolve, reject) => {
         Papa.parse(file, {
